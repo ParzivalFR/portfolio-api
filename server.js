6735b1720f21b7a149288cd11c32d08d0444481a
ParzivalFR <parzivaleu@gmail.com>
2024-05-13 01:32:15 +0200

    Change http to https

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const http = require("http");
+const https = require("https");
 const app = require("./app");
 
 const normalizePort = (val) => {
@@ -41,7 +41,7 @@ const errorHandler = (error) => {
   }
 };
 
-const server = http.createServer(app);
+const server = https.createServer(app);
 
 server.on("error", errorHandler);
 
